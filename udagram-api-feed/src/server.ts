5c00f4ac84cac889e9f5b2c598983cd588ec5c74
Viet Vo Hoang <vietvohoang@Viets-MacBook-Pro.local>
2022-11-03 10:42:35 +0700

    debug

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -10,6 +10,7 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
+  console.log('config: ', config)
   await sequelize.addModels(V0_FEED_MODELS);
 
   console.debug("Initialize database connection...");
